a3cd3f5f4fcf4cb74c08773e0b14e6d4dad8e2b5
dengying826 <467847330@qq.com>
2019-05-11 19:58:08 +0800

    0511 math module test

---
+++ demo/hello.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+color = input("What's your favorite color ? ")
+msg = f'{name} likes {color}'  # 格式化字符串
+print(msg)
---
+++ demo/math_module.py
@@ -0,0 +1,6 @@
+import math  # 数学模块
+x = 2.9
+print(abs(x))
+print(round(x))
+print(math.floor(x))
+print(math.ceil(x))
