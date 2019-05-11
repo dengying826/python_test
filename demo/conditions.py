56733b518709abaf27477dffef3451d2e65b1e0b
dengying826 <467847330@qq.com>
2019-05-11 20:28:50 +0800

    0511 conditions test

---
+++ demo/conditions.py
@@ -0,0 +1,20 @@
+# is_hot = False
+# is_cold = True
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a col d day")
+#     print("Wear warm clothes")
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+
+has_good_credit = True
+price = 1000000
+if has_good_credit:
+    down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+print(f"Down payment: ${down_payment}")
